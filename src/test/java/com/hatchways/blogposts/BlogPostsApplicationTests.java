5f09be0e1c32c244a88ca54f0756d0281886e523
Anshoo Arora <anshooarora@gmail.com>
2024-03-13 10:06:59 -0400

    unit tests: initial contextLoads

---
+++ src/test/java/com/hatchways/blogposts/BlogPostsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hatchways.blogposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BlogPostsApplicationTests {
+
+    @Test
+    public void contextLoads() { }
+
+}
